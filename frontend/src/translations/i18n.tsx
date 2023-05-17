8ac0ec06d0cc27b5dc1b9ff7f85f7b9c448d67d2
Renato <rkauric@vvg.hr>
2023-05-17 19:46:22 +0200

    Added translations

--- frontend/src/translations/i18n.tsx
+++ frontend/src/translations/i18n.tsx
@@ -1,5 +1,6 @@
-import de from './locales/de.json';
 import en from './locales/en.json';
+import de from './locales/de.json';
+import es from './locales/es.json';
 
 const resources = {
   en: {
@@ -7,6 +8,9 @@ const resources = {
   },
   de: {
     translation: de,
+  },
+  es: {
+    translation: es,
   },
 };
 
