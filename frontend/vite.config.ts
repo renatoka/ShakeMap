8a155a1e801e7b935e1098df5657488edd1f1076
Renato <rkauric@vvg.hr>
2023-05-17 19:48:40 +0200

    Expose host on development

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    host: true,
+    host: process.env.NODE_ENV == 'development' ? true : false,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
