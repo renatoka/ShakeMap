964ffa0a3f7b8824810dfcce3498486129b1014f
Renato <rkauric@vvg.hr>
2023-05-20 11:39:08 +0200

    Updated translation, added locale date format

--- frontend/src/translations/i18n.tsx
+++ frontend/src/translations/i18n.tsx
@@ -20,6 +20,11 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 
+const userLocale = navigator.language;
+if (userLocale) {
+  localStorage.setItem('locale', userLocale);
+}
+
 i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
   fallbackLng: 'en',
   resources,
