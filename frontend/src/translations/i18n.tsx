587a76b42e8b6022b0b024c8a36010741e60dc50
Renato <rkauric@vvg.hr>
2023-05-21 11:49:51 +0200

    refactor: added new translations

--- frontend/src/translations/i18n.tsx
+++ frontend/src/translations/i18n.tsx
@@ -1,5 +1,9 @@
-import en from './locales/en.json';
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
 import de from './locales/de.json';
+import en from './locales/en.json';
 import es from './locales/es.json';
 
 const resources = {
@@ -14,21 +18,19 @@ const resources = {
   },
 };
 
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-
-import LanguageDetector from 'i18next-browser-languagedetector';
-import Backend from 'i18next-http-backend';
+i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
+  fallbackLng: 'en',
+  resources,
+  lng: 'en',
+});
 
 const userLocale = navigator.language;
 if (userLocale) {
   localStorage.setItem('locale', userLocale);
 }
 
-i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
-  fallbackLng: 'en',
-  resources,
-  lng: 'en',
-});
+if (localStorage.getItem('language') !== null) {
+  i18n.changeLanguage(localStorage.getItem('language') || 'en');
+}
 
 export default i18n;
