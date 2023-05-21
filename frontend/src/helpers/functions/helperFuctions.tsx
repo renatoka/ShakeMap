eb63a7cea2f016a61bf544e328b4448cc9110a1e
Renato <rkauric@vvg.hr>
2023-05-21 11:50:37 +0200

    feature: joi validation config

--- frontend/src/helpers/functions/helperFuctions.tsx
+++ frontend/src/helpers/functions/helperFuctions.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Joi from 'joi';
 
 export const getEarthquakes = async (limit: number) => {
   try {
@@ -14,4 +15,21 @@ export const getEarthquakes = async (limit: number) => {
   } catch (error) {
     return error;
   }
+};
+
+export const validate = (data: any) => {
+  const schema = Joi.object({
+    firstName: Joi.string().required().label('First Name'),
+    lastName: Joi.string().required().label('Last Name'),
+    email: Joi.string()
+      .required()
+      .email({ tlds: { allow: false } })
+      .label('Email'),
+  });
+  const { error } = schema.validate(data);
+  if (error) {
+    console.log(error);
+    return false;
+  }
+  return true;
 };
