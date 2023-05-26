a543f627476b212a6c5ec15c4df0e7079ed47ba1
Renato <rkauric@vvg.hr>
2023-05-26 14:26:56 +0200

    fix: add module as dependancy

--- src/earthquakes/earthquakes.module.ts
+++ src/earthquakes/earthquakes.module.ts
@@ -4,10 +4,18 @@ import { EarthquakesController } from './earthquakes.controller';
 import { EarthquakesService } from './earthquakes.service';
 import { CronService } from 'src/cron/cron.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { MailerService } from 'src/mailer/mailer.service';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
   imports: [HttpModule],
   controllers: [EarthquakesController],
-  providers: [EarthquakesService, PrismaService, CronService],
+  providers: [
+    EarthquakesService,
+    PrismaService,
+    CronService,
+    MailerService,
+    UsersService,
+  ],
 })
 export class EarthquakesModule {}
