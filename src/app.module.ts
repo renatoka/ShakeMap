5c2e2d2b61fdd5c8f9a82dd930395753394a4f18
Renato <rkauric@vvg.hr>
2023-05-27 21:27:59 +0200

    feature: add jwt package and import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,12 +7,21 @@ import { EarthquakesModule } from './earthquakes/earthquakes.module';
 import { MapboxTokenModule } from './mapbox-token/mapbox-token.module';
 import { UsersModule } from './users/users.module';
 import { MailerService } from './mailer/mailer.service';
+import { JwtModule } from '@nestjs/jwt';
+
+const configService = new ConfigService();
+
 @Module({
   imports: [
     EarthquakesModule,
     MapboxTokenModule,
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    JwtModule.register({
+      global: true,
+      signOptions: { expiresIn: '1h' },
+      secret: configService.get<string>('JWT_SECRET'),
     }),
     ScheduleModule.forRoot(),
     UsersModule,
