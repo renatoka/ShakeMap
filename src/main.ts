c9fde11b17f88841462538407d4d4326435e5d92
Renato <rkauric@vvg.hr>
2023-07-29 18:07:17 +0200

    refactor: code fix

--- src/main.ts
+++ src/main.ts
@@ -14,15 +14,11 @@ async function bootstrap() {
   );
   await app.listen(3001);
   try {
-    const start = new Date(new Date().setHours(0, 0, 0, 0)).toISOString(); // 00:00:00
-    const end = new Date().toISOString(); // now
-
     await earthquakesService.fetchEarthquakes({
-      start: start,
-      end: end,
-      limit: null,
+      start: new Date(new Date().setHours(0, 0, 0, 0)).toISOString(),
+      end: new Date().toISOString(),
+      limit: 1000,
     });
-    Logger.log(`Fetched earthquakes from ${start} to ${end}`, 'Bootstrap');
   } catch (error) {
     Logger.error(error, 'Bootstrap');
   }
